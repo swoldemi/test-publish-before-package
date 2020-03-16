b25669714996f64bcca8837288e309a3541416d5
Simon Woldemichael <swoldemichael@acm.org>
2020-03-16 15:34:38 -0500

    Break more stuff

--- publish.sh
+++ publish.sh
@@ -25,12 +25,13 @@ publish_all_regions()
         echo Deploying to region $REGION
         sam publish --region $REGION --template packaged.yaml # Publish before package
 	    sam package --template-file template.yaml --s3-bucket $BUCKET_PREFIX-$REGION --output-template-file packaged.yaml
+        aws serverlessrepo put-application-policy \
+            --region us-east-1 \
+            --application-id arn:aws:serverlessrepo:us-east-1:273450712882:applications/$APPLICATION \
+            --statements Principals=*,Actions=Deploy
     done
 
-    aws serverlessrepo put-application-policy \
-        --region us-east-1 \
-        --application-id arn:aws:serverlessrepo:us-east-1:273450712882:applications/$APPLICATION \
-        --statements Principals=*,Actions=Deploy
+
 }
 
 echo "On branch `basename $CODEBUILD_WEBHOOK_HEAD_REF`"
@@ -40,3 +41,6 @@ then
 else
     echo Skipping publish
 fi
+
+
+        sam publish --region ap-south-1 --template packaged.yaml # Publish before package
