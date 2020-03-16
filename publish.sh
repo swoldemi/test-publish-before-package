3e3c0726e4862307f20c46084c9ccce054d9069d
Simon Woldemichael <swoldemichael@acm.org>
2020-03-16 15:42:41 -0500

    Remove old bucket links

--- publish.sh
+++ publish.sh
@@ -12,15 +12,13 @@ publish_all_regions()
     for REGION in "${REGIONS[@]}"
     do  
         echo Deploying to region $REGION
-        sam publish --region $REGION --template packaged.yaml # Publish before package
 	    sam package --template-file template.yaml --s3-bucket $BUCKET_PREFIX-$REGION --output-template-file packaged.yaml
+        sam publish --region $REGION --template packaged.yaml 
         aws serverlessrepo put-application-policy \
             --region us-east-1 \
             --application-id arn:aws:serverlessrepo:us-east-1:273450712882:applications/$APPLICATION \
             --statements Principals=*,Actions=Deploy
     done
-
-
 }
 
 echo "On branch `basename $CODEBUILD_WEBHOOK_HEAD_REF`"
