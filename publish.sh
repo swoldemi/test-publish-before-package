6af2bb7be873352f7a0f68d1318797de0368f45c
Simon Woldemichael <swoldemichael@acm.org>
2020-03-16 15:51:22 -0500

    force push

--- publish.sh
+++ publish.sh
@@ -4,7 +4,10 @@ BUCKET_PREFIX=swoldemi
 APPLICATION=test-publish-before-package
 ACCOUNT=273450712882
 declare -a REGIONS=(
+    "ap-east-1"
     "ap-south-1"
+    "ap-northeast-2"
+    "ap-southeast-1"
 )
 
 publish_all_regions()
@@ -12,10 +15,10 @@ publish_all_regions()
     for REGION in "${REGIONS[@]}"
     do  
         echo Deploying to region $REGION
-	    sam package --template-file template.yaml --s3-bucket $BUCKET_PREFIX-$REGION --output-template-file packaged.yaml
         sam publish --region $REGION --template packaged.yaml 
+	    sam package --template-file template.yaml --s3-bucket $BUCKET_PREFIX-$REGION --output-template-file packaged.yaml
         aws serverlessrepo put-application-policy \
-            --region us-east-1 \
+            --region $REGION \
             --application-id arn:aws:serverlessrepo:us-east-1:273450712882:applications/$APPLICATION \
             --statements Principals=*,Actions=Deploy
     done
